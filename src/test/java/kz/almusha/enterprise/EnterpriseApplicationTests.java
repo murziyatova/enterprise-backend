cea02bd598783386b73eb400984392e5a6b6faa1
Alma Murziyatova <almamurziyatova@gmail.com>
2020-03-15 15:32:22 +0600

    init

---
+++ src/test/java/kz/almusha/enterprise/EnterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.almusha.enterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
